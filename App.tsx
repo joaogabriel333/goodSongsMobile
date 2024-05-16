8953657b91eb25516bd92446c6be742c8ab44281
joaogabriel333 <joao.gabriel.souzaiclououd.com@gmail.com>
2024-05-16 15:26:48 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React, { JSXElementConstructor } from 'react';
+import { Text, View } from 'react-native';
+
+import CadastroMusica from './src/screens/cadastroMusica';
+import SignIn from './src/screens/sign';
+
+function App():JSX.Element{
+
+  return(
+    <SignIn/>
+  );
+}
+
+
+
+export default App;
